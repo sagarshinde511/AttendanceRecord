cd3104a7656cfa901f75f735cd3ea1fdfefce4bf
sagarshinde511 <amplifierelectronics9@gmail.com>
2025-06-16 21:52:57 +0530

    Update Attendance.py

--- Attendance.py
+++ Attendance.py
@@ -91,6 +91,9 @@ with tabs[1]:
 
     # Reorder
     pivot_df = pivot_df[['RollNo', 'StudentName'] + sorted(pivot_df.columns[2:])]
+    # Sort by RollNo ascending
+    pivot_df = pivot_df.sort_values(by="RollNo")
+
 
     # Display with colors
     styled_df = pivot_df.style.applymap(highlight_status, subset=pivot_df.columns[2:])
